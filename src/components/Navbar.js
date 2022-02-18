67a1d9d6dda71cb59d5843afb13d56581ab4645a
Michael Antoni <michaelantoni.cs@gmail.com>
2022-02-19 02:24:33 +0800

    update navbat

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,17 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
         <div className="container">
-            <a className="navbar-brand" href="/">Coding Assestment</a>
+            <a className="navbar-brand" href="/">Home</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link" aria-current="page" href="#">Quiz A</a>
+                        <a className="nav-link" aria-current="page" href="#">Quiz-A</a>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to={`/quiz-b`}>Quiz B</Link>
+                        <Link className="nav-link" to={`/quiz-b`}>Quiz-B</Link>
                     </li>
                 </ul>
             </div>
