7f399ab1ea8d3121a354d3c499626dc8ac4c3e75
Michael Antoni <michaelantoni.cs@gmail.com>
2022-02-19 05:56:39 +0800

    finishing touches in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ function Home() {
   return (
     <div className='mt-3'>
         <div className="d-flex justify-content-center">
-            <div className="col-6">
-                <h5>Created by: Michael B. Antoni</h5>
-                
+            <div className="col-6 text-center">
+                <h5 className='display-5 mb-4'>Created By: Michael B. Antoni</h5>
+                <h5>Source code: <a href="https://github.com/m-antoni/coding-quiz" target="_blank">https://github.com/m-antoni/coding-quiz</a></h5>
             </div>
         </div>
     </div>
